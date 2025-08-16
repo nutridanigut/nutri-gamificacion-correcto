8082ea2d0351d6dc23d24c8752c73970ec97d2b6
nutridanigut <nutridanigut@gmail.com>
2025-08-16 10:46:22 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './style.css'   // 👈 agrega esta línea si falta
+import './style.css'   
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
