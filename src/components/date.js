cad1661f154d0cd1a34ee69f717e0abe95f2d18b
nutridanigut <nutridanigut@gmail.com>
2025-08-18 17:23:05 -0400

    Delete src/components/date.js

--- src/components/date.js
+++
@@ -1,9 +0,0 @@
-// src/utils/date.js
-export const todayKey  = () => new Date().toDateString();
-export const todayIdx  = () => new Date().getDay();
-export const WEEK_LABELS = ["D", "L", "M", "M", "J", "V", "S"];
-
-export const hhmmToMin = (t) => {
-  const [h, m] = t.split(":").map(Number);
-  return h * 60 + m;
-};
